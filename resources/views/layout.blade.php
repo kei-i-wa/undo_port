eb53c6b9d91e21a65aeebdf55508a949bc58b7fd
kei-i-wa <kei.kumarg@gmail.com>
2022-05-01 06:56:41 +0000

    中央よせ

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,14 +3,19 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
         <title>ログイン機能付健康管理サービス @yield('title')</title>
     </head>
     <body>
-       <i class="fa-solid fa-right-to-bracket"></i>
-  
+      <div class="container">
+        <div class="row">
+          <!--<div class="col-lg-8"></div>-->
+       <!-- <i class="bi bi-box-arrow-in-right"></i>-->
+       <!--<i class="fa-solid fa-right-to-bracket"></i>-->
 @yield('contets')
-
+      </div>
+      </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>    </body>
 </html>
